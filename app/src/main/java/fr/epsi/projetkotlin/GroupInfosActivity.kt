374746c231f30b3e8fdede7ea99dd3d6e9ce064b
Thibault <72927736+ThibaultBarral@users.noreply.github.com>
2023-02-20 20:28:38 +0100

    Dev/thibault (#6)
    * splash activity / home activty
    
    * create infoActivity

--- app/src/main/java/fr/epsi/projetkotlin/GroupInfosActivity.kt
+++ app/src/main/java/fr/epsi/projetkotlin/GroupInfosActivity.kt
@@ -3,14 +3,16 @@ package fr.epsi.projetkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class GroupInfosActivity : MainActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_group_infos)
+        if(intent.extras!=null){
+            val title=intent.extras!!.getString("title","Epsi")
+            setHeaderTxt(title)
+        }
         showBack()
-        val buttonStudent1 = findViewById<Button>(R.id.buttonStudent1)
-        val buttonStudent2 = findViewById<Button>(R.id.buttonStudent3)
-        val buttonStudent3 = findViewById<Button>(R.id.buttonStudent3)
     }
 }
