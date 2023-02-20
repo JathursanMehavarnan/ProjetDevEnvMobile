40b84a41a9ca025d6e5432d67fd80eaa04031238
Thibault Barral <thibault.barral3347@gmail.com>
2023-02-20 10:36:07 +0100

    feat : create layout_header & buttons activity_home

--- app/src/main/java/fr/epsi/projetkotlin/HomeActivity.kt
+++ app/src/main/java/fr/epsi/projetkotlin/HomeActivity.kt
@@ -3,9 +3,12 @@ package fr.epsi.projetkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : MainActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setHeaderTxt("Epsi")
     }
+
 }
--- app/src/main/java/fr/epsi/projetkotlin/MainActivity.kt
+++ app/src/main/java/fr/epsi/projetkotlin/MainActivity.kt
@@ -5,8 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +16,11 @@ class MainActivity : AppCompatActivity() {
             startActivity(newIntent)
             finish()
         },2000)
+
+    }
+
+    fun setHeaderTxt(txt:String) {
+        val textViewTitle = findViewById<TextView>(R.id.textViewTitle)
+        textViewTitle.setText(txt)
     }
 }
